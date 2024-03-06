4704596885067a11f6215d9fed43c8ed040760eb
Navid Shokoufeh <navid.shekofeh@gmail.com>
2024-03-06 16:56:28 +0330

    [add] implement unique id

--- lib/src/repo.dart
+++ lib/src/repo.dart
@@ -1,4 +1,3 @@
-import 'package:device_imei/device_imei.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:unique_identifier/unique_identifier.dart';
 import 'package:voipmax/src/bloc/bloc.dart';
@@ -6,15 +5,15 @@ import 'package:voipmax/src/data/models/sip_server_model.dart';
 
 class MyTelRepo extends Bloc {
   SIPServerModel? sipServer;
-  String? imei;
+  String? uniqueDeviceId;
+  String? fcmToken;
 
   Future getIMEI() async {
     try {
       Permission.phone.request();
-      imei = await UniqueIdentifier.serial;
-      print(imei);
+      uniqueDeviceId = await UniqueIdentifier.serial;
     } catch (e) {
-      imei = null;
+      uniqueDeviceId = null;
     }
   }
 
