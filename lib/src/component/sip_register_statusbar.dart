90e6e10c9ab1937031395a847d55b687eba1660e
Navid Shokoufeh <navid.shekofeh@gmail.com>
2024-03-12 23:59:38 +0330

    [edit] dynamic statusbar

--- lib/src/component/sip_register_statusbar.dart
+++ lib/src/component/sip_register_statusbar.dart
@@ -10,30 +10,31 @@ class SipRegisterStatusBar extends StatelessWidget {
   Widget build(BuildContext context) {
     SIPBloc sipController = Get.find();
     return Padding(
-      padding: const EdgeInsets.only(right: 8.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          sipController.registrationStatus.value ==
-                  MytelValues.deviceRegisteredStatus
-              ? Container(
-                  height: 20,
-                  width: 20,
-                  decoration: BoxDecoration(
-                      color: Colors.greenAccent,
-                      borderRadius: BorderRadius.circular(3)),
-                )
-              : Container(
-                  height: 20,
-                  width: 20,
-                  decoration: BoxDecoration(
-                      color: Colors.redAccent,
-                      borderRadius: BorderRadius.circular(3)),
-                ),
-          Text(sipController.registrationStatus.value)
-        ],
-      ),
-    );
+        padding: const EdgeInsets.only(right: 8.0),
+        child: Obx(
+          () => Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              sipController.registrationStatus.value ==
+                      MytelValues.deviceRegisteredStatus
+                  ? Container(
+                      height: 20,
+                      width: 20,
+                      decoration: BoxDecoration(
+                          color: Colors.greenAccent,
+                          borderRadius: BorderRadius.circular(3)),
+                    )
+                  : Container(
+                      height: 20,
+                      width: 20,
+                      decoration: BoxDecoration(
+                          color: Colors.redAccent,
+                          borderRadius: BorderRadius.circular(3)),
+                    ),
+              Text(sipController.registrationStatus.value)
+            ],
+          ),
+        ));
   }
 }
